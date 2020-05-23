c32c140659dce1c2511f151c6800f6cc1e33cc7f
nyasingajeff <nyasingajeff@gmail.com>
2020-05-23 20:29:24 +0300

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+    # to start first by cheking the parameters
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keyss[:name])
+        devise_parameter_sanitizer.permit(:account_update, keyss[:name])
+                
+    end
+end
