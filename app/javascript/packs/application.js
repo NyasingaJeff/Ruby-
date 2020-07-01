80ddee6c711180c10ee146d14dabb1ccd41b673f
nyasingajeff <nyasingajeff@gmail.com>
2020-07-02 00:52:45 +0300

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,11 @@
 // that code so it'll be compiled.
 //= require jquery
 //= require bootstrap-sprockets
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require ("jquery3")
+//=require("@rails/ujs").start()
+//=require("turbolinks").start()
+//=require("@rails/activestorage").start()
+//=require("channels")
+//=require ("popper")
 //require("toastr")
 
 
